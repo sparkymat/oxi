098e1fa6be8070a0133a53a0b3514e44ed4bed5c
Ajith Hussain <ajith.hussain@fastmail.com>
2020-07-03 20:32:11 +1000

    Added kingping + arg

--- oxi.go
+++ oxi.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/alecthomas/kingpin"
+)
+
+var (
+	app = kingpin.New("oxi", "oxi is a commandline HTTP client")
+	url = app.Arg("url", "URL to fetch").String()
+)
+
 func main() {
+	if _, err := app.Parse(os.Args[1:]); err != nil {
+		panic(err)
+	}
+
+	if url != nil && *url != "" {
+		fmt.Printf("GET %s", *url)
+	} else {
+		fmt.Println("Entering interactive mode")
+	}
 }
